4b6559900edaef2effd3992174fc072345e47aa6
dianingsps <dianingsukmapilia@gmail.com>
2019-05-16 08:38:57 +0700

    Main Female Male

---
+++ src/Female.js
@@ -0,0 +1,53 @@
+import React, {Component } from 'react';
+    
+class Female extends Component{
+        constructor(props){
+            super(props);
+            this.state = {
+                items: []
+            };
+        }
+    
+        componentDidMount(){
+            fetch("https://randomuser.me/api/?gender=female")
+            .then(res => res.json())
+            .then(parsedJSON => parsedJSON.results.map(data => (
+                {
+                        id:`${data.id.name}`, 
+                        location:`${data.location.state}`,
+                        thumbnail: `${data.picture.large}`
+                }
+            )))
+            .then(items => this.setState({
+                items,
+                isloaded: false
+            }))
+            .catch(error => console.log('parsing failed', error))
+        }
+    render(){
+        const {items} = this.state;
+        return (
+            <div className="boxWhite">
+            {
+                items.length > 0 ? items.map(item => {
+                    const {id, firstName, lastName, location, thumbnail} = item;
+                    return (
+                        <div key={id} className="bgCircle">
+                        <center>
+                            <img src={thumbnail} alt={firstName} className="circle"/>
+                        </center>
+                        <br />
+                        <div className="ctr">
+                        {firstName} {lastName}<br />
+                        {location}
+                        </div>
+                        </div>
+                    );
+                }) : null
+            }
+            </div>
+        );
+    }
+}
+
+export default Female;
--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,30 @@
 import React, {Component } from 'react';
-import {Route, NavLink,HashRouter } from "react-router-dom";
+import {Route, NaLink, HashRouter} from "react-router-dom";
 import Home from "./Home";
+import Male from "./Male";
+import Female from "./Female";
 
-class Main extends Component{
+class Main extends Component {
     render(){
         return (
             <HashRouter>
-            <div>
-            <h1 className="title"> Fetching Data Api </h1>
-            <ul className="header">
-            <li><NavLink exact to="/">Home</NavLink></li>
-
-            </ul>
-            <div className="content">
-            <Route exact path="/" component={Home}/>
-            </div>
-            </div>
-            </HashRouter>
+                <div>
+                    <h1 className="title">Fetching Data Api</h1>
+                    <ul className="header">
+                        <li><NavLink exact to="/">Home<NavLink></li>
+                        <li><NavLink exact to="/male">Male<NavLink></li>
+                        <li><NavLink exact to="/female">Female<NavLink></li>
+                    </ul>
+                <div className="content">
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/male" component={Male}/>
+                    <Route exact path="/female" component={Female}/>
+                </div>
+                </div>
+                </HashRouter>
         );
     }
 }
+
 
 export default Main;
